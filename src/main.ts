9de7a54d5ca91e55706dc8a675acd78e3c184321
John Mason <87030324+simkidd@users.noreply.github.com>
2024-08-02 00:16:21 +0100

    added dotenv and bcryptjs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
