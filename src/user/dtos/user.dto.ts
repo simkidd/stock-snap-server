7eeb2669ce804670f5ebb9156206f0cca9773fa3
John Mason <87030324+simkidd@users.noreply.github.com>
2024-08-08 10:40:52 +0100

    chore: filter users

--- src/user/dtos/user.dto.ts
+++ src/user/dtos/user.dto.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserRole } from '@prisma/client';
-import { IsEmail, IsEnum, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateUserInput {
   @ApiProperty({ example: 'John Doe' })
@@ -43,4 +49,22 @@ export class UpdateUserInput extends CreateUserInput {
   @ApiProperty({ example: 'user-id' })
   @IsString()
   id: string;
+}
+
+export class FilterUsersInput {
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsNumber()
+  skip?: number;
+
+  @IsOptional()
+  @IsNumber()
+  limit?: number;
+
+  @IsOptional()
+  @IsNumber()
+  page?: number;
 }
